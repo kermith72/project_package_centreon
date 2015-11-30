8b9bc6c6da8c8c70ff94d77c3199a0040039cfac
Eric Coquard <eric.coquard@gmail.com>
2015-11-30 12:14:50 +0100

    update centreon-broker 2.10.2

--- centreon-broker-2.10.2/build/package.cmake
+++ centreon-broker-2.10.2/build/package.cmake
@@ -31,18 +31,18 @@ if (WITH_PACKAGE_SH
     OR WITH_PACKAGE_NSIS)
   # Default settings.
   set(CPACK_PACKAGE_VENDOR "Centreon")
+  set (CPACK_PACKAGE_NAME "centreon-broker")
   set(CPACK_PACKAGE_VERSION_MAJOR "${CENTREON_BROKER_MAJOR}")
   set(CPACK_PACKAGE_VERSION_MINOR "${CENTREON_BROKER_MINOR}")
   set(CPACK_PACKAGE_VERSION_PATCH "${CENTREON_BROKER_PATCH}")
   set(CPACK_PACKAGE_DESCRIPTION_SUMMARY
-    "Centreon Broker is a monitoring addon fully compatible with Nagios and Centreon Engine.")
+    "Centreon Broker offers a new effective way to store your Monitoring Engine events in a database.")
   set(CPACK_PACKAGE_FILE_NAME
     "centreon-broker-${CENTREON_BROKER_VERSION}")
   set(CPACK_PACKAGE_INSTALL_DIRECTORY "centreon-broker")
   set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE")
   set(CPACK_PACKAGE_CONTACT
-    "Matthieu Kermagoret <mkermagoret@centreon.com>")
-
+    "Eric Coquard <eric.coquard@gmail.com>")
   # Generators.
   unset(PACKAGE_LIST)
   if (WITH_PACKAGE_SH)
@@ -61,6 +61,14 @@ if (WITH_PACKAGE_SH
     list(APPEND CPACK_GENERATOR "DEB")
     list(APPEND PACKAGE_LIST "DEB package (.deb)")
     set(CPACK_DEBIAN_PACKAGE_SECTION "net")
+    set (CPACK_DEBIAN_PACKAGE_DEPENDS "centreon-clib (>= 1.4.2)")
+    set (CPACK_DEBIAN_PACKAGE_RECOMMENDS "centreon-engine (>= 1.4.15)")
+    configure_file("${PROJECT_SOURCE_DIR}/script/debian/postinst.in" "${PROJECT_SOURCE_DIR}/script/debian/postinst")
+    set (CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+    set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA
+      "${PROJECT_SOURCE_DIR}/script/debian/postinst"
+      "${PROJECT_SOURCE_DIR}/script/debian/prerm")
+    set (CPACK_DEBIAN_PACKAGE_PRIORITY "extra")
   endif ()
   if (WITH_PACKAGE_RPM)
     list(APPEND CPACK_GENERATOR "RPM")
